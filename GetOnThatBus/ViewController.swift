9bc65fce999444deda65af02ef8169bf7a0439a3
Kelvin <043655@gmail.com>
2020-01-27 14:06:27 -0500

    Initial Commit

---
+++ GetOnThatBus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetOnThatBus
+//
+//  Created by Kelvin Vazquez on 1/27/20.
+//  Copyright © 2020 Kelvin Vazquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
